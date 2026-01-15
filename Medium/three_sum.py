87d93e8bcb159a33c38a65349cfd17c64e593b03
Azhar304 <azharmehmood666666@gmail.com>
2026-01-15 11:54:01 +0500

    Two pointer solution also added

--- Medium/three_sum.py
+++ Medium/three_sum.py
@@ -1,20 +1,57 @@
-class Solution:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
-        n = len(nums)
-        combinations = []
+## O(n**3) solution
+# class Solution:
+#     def threeSum(self, nums: list[int]) -> list[list[int]]:
+#         n = len(nums)
+#         combinations = []
 
-        if n == 0:
-            return "Empty list"
+#         if n == 0:
+#             return "Empty list"
 
-        for i in range(n):  
-            for j in range(i+1, n): 
-                for k in range(j+1, n):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        combinations.append([nums[i], nums[j], nums[k]])
+#         for i in range(n):  
+#             for j in range(i+1, n): 
+#                 for k in range(j+1, n):
+#                     if nums[i] + nums[j] + nums[k] == 0:
+#                         combinations.append([nums[i], nums[j], nums[k]])
 
         
-        unique = set(tuple(sorted(x)) for x in combinations)
-        return [list(x) for x in unique]
+#         unique = set(tuple(sorted(x)) for x in combinations)
+#         return [list(x) for x in unique]
 
+# nums = [-1,0,1,2,-1,-4]
+# print(Solution().threeSum(nums))
+
+#two pointer solution 
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        combinations = []
+        n = len(nums)
+
+        for i in range(n):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+
+            left = i+1
+            right = n-1
+
+            while left < right :
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    combinations.append([nums[i], nums[left], nums[right]])
+                    left+=1
+                    right-=1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                
+                elif total < 0 :
+                    left+=1
+                else:
+                    right-=1
+
+        return combinations
+    
 nums = [-1,0,1,2,-1,-4]
 print(Solution().threeSum(nums))
