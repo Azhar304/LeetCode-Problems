b17354ed516ea6c8e320b00e8f9d07e2e090fd4b
Azhar304 <azharmehmood666666@gmail.com>
2026-01-16 12:04:25 +0500

    Fix three_sum function for edge cases

--- Medium/three_sum.py
+++ Medium/three_sum.py
@@ -1,24 +1,21 @@
-## O(n**3) solution
-# class Solution:
-#     def threeSum(self, nums: list[int]) -> list[list[int]]:
-#         n = len(nums)
-#         combinations = []
+# O(n**3) solution
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        n = len(nums)
+        combinations = []
 
-#         if n == 0:
-#             return "Empty list"
+        if n == 0:
+            return "Empty list"
 
-#         for i in range(n):  
-#             for j in range(i+1, n): 
-#                 for k in range(j+1, n):
-#                     if nums[i] + nums[j] + nums[k] == 0:
-#                         combinations.append([nums[i], nums[j], nums[k]])
+        for i in range(n):  
+            for j in range(i+1, n): 
+                for k in range(j+1, n):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        combinations.append([nums[i], nums[j], nums[k]])
 
         
-#         unique = set(tuple(sorted(x)) for x in combinations)
-#         return [list(x) for x in unique]
-
-# nums = [-1,0,1,2,-1,-4]
-# print(Solution().threeSum(nums))
+        unique = set(tuple(sorted(x)) for x in combinations)
+        return [list(x) for x in unique]
 
 #two pointer solution 
 from typing import List
