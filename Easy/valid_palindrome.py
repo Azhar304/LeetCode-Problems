e08539ad87efaf8d53dec82e23aa6a1267935ce0
Azhar304 <azharmehmood666666@gmail.com>
2026-02-14 19:09:26 +0500

    Valid palindrome checker ins string

---
+++ Easy/valid_palindrome.py
@@ -0,0 +1,30 @@
+"""Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ """
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        rmv = [',', '!', ':', " "]
+   
+        for ch in rmv:
+            s = s.replace(ch, "").lower()
+        
+        return s == s[::-1]
+    
+s = "A man, a plan, a canal: Panama"
+print(Solution().isPalindrome(s))
