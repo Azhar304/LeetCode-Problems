d7ba2a1da775adee6357adb0d4ca19609d3a0225
Azhar304 <azharmehmood666666@gmail.com>
2026-02-14 19:28:20 +0500

    Added two pointer solution

--- Easy/valid_palindrome.py
+++ Easy/valid_palindrome.py
@@ -28,3 +28,27 @@ class Solution:
         return cleaned == cleaned[::-1]
     
 
+#two pointer solution
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        left = 0
+        right = len(s)-1
+        while left < right:
+
+        # moving left pointer
+            while left < right and not s[left].isalnum():
+                    left += 1
+
+            # moving right pointer
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower()!= s[right].lower():
+                return False
+            
+            left+=1
+            right-=1
+
+        return True
